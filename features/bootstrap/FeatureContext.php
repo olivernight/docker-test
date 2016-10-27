d63163a2b4d7b6c246a39ce3b1190b6684fc6ef0
olivernight <work@gallas.me>
2016-10-27 07:18:04 +0200

    Tests passed

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,8 +1,6 @@
 <?php
 
 use Behat\Behat\Context\Context;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
 
 /**
  * Defines application features from the specific context.
@@ -16,7 +14,61 @@ class FeatureContext implements Context
      * You can also pass arbitrary arguments to the
      * context constructor through behat.yml.
      */
+    private $shelf;
+    private $basket;
+
     public function __construct()
     {
+        $this->shelf = new Shelf();
+        $this->basket = new Basket($this->shelf);
     }
+
+    /**
+     * @Given there is a :arg1, which costs £:arg2
+     */
+    public function thereIsAWhichCostsPs($product, $price)
+    {
+        $this->shelf->setProductPrice($product, $price);
+    }
+
+    /**
+     * @When I add the :product to the basket
+     */
+    public function iAddTheToTheBasket($product)
+    {
+        $this->basket->addBasket($product);
+    }
+
+    /**
+     * @Then I should have :count product in the basket
+     */
+    public function iShouldHaveProductInTheBasket($count)
+    {
+        PHPUnit_Framework_Assert::assertCount(
+          intval($count),
+          $this->basket->getProducts()
+      );
+    }
+
+    /**
+     * @Then the overall basket price should be £:arg1
+     */
+    public function theOverallBasketPriceShouldBePs($price)
+    {
+        PHPUnit_Framework_Assert::assertSame(
+            floatval($price),
+            $this->basket->getTotalPrice()
+        );
+    }
+
+  /**
+   * @Then I should have :count products in the basket
+   */
+  public function iShouldHaveProductsInTheBasket($count)
+  {
+      PHPUnit_Framework_Assert::assertCount(
+      intval($count),
+      $this->basket->getProducts()
+    );
+  }
 }
